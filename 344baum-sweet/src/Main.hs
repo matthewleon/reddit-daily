f41c5bff67addb0d374238e86d0502acb7aa0991
Matthew Leon <ml@matthewleon.com>
2017-12-26 11:14:53 -0500

    344baum-sweet: use vectors

--- 344baum-sweet/src/Main.hs
+++ 344baum-sweet/src/Main.hs
@@ -1,18 +1,18 @@
 module Main where
 
-import qualified Data.Array as A
+import qualified Data.Vector as V
 
 main :: IO ()
-main = print . A.elems . fmap fromEnum . bsSeq =<< readLn
+main = print . fmap fromEnum . bsSeq =<< readLn
 
-bsSeq :: Int -> A.Array Int Bool
+bsSeq :: Int -> V.Vector Bool
 bsSeq sz = bs
   where
-  bs :: A.Array Int Bool
-  bs = A.listArray (0, sz - 1) $ True : (b <$> [1..])
+  bs :: V.Vector Bool
+  bs = V.fromListN sz $ True : (b <$> [1..])
 
   b :: Int -> Bool
   b n =
     if n `mod` 4 == 0
-      then bs A.! (n `div` 4)
-      else odd n && (bs A.! ((n - 1) `div` 2))
+      then bs V.! (n `div` 4)
+      else odd n && (bs V.! ((n - 1) `div` 2))
