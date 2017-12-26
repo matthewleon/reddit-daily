ead0c281691161ba06012bd5413301a787961188
Matthew Leon <ml@matthewleon.com>
2017-12-26 10:49:19 -0500

    344baum-sweet

---
+++ 344baum-sweet/src/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import qualified Data.Array as A
+
+main :: IO ()
+main = print . A.elems $ fromEnum <$> bsSeq 200
+
+bsSeq :: Int -> A.Array Int Bool
+bsSeq sz = bs
+  where
+  bs :: A.Array Int Bool
+  bs = A.listArray (0, sz - 1) $ True : (b <$> [1..])
+
+  b :: Int -> Bool
+  b n =
+    if n `mod` 4 == 0
+      then bs A.! (n `div` 4)
+      else odd n && (bs A.! ((n - 1) `div` 2))
