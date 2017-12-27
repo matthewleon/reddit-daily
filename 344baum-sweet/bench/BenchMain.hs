3377c0afe1fbe15b20e40796f485a22295e839c4
Matthew Leon <ml@matthewleon.com>
2017-12-26 19:04:49 -0500

    report

--- 344baum-sweet/bench/BenchMain.hs
+++ 344baum-sweet/bench/BenchMain.hs
@@ -1,17 +1,21 @@
 module Main where
 
 import Criterion.Main
+import Criterion.Types
 import BaumSweet (bsSeq)
 
+config :: Config
+config = defaultConfig { reportFile = Just "report.html" }
+
 main :: IO ()
-main = defaultMain [
+main = defaultMainWith config [
   bgroup "bsSeq" [ bench "1"  $ nf bsSeq 1
                  , bench "10" $ nf bsSeq 10
                  , bench "100" $ nf bsSeq 100
                  , bench "1000" $ nf bsSeq 1000
                  , bench "10000" $ nf bsSeq 10000
                  , bench "100000" $ nf bsSeq 100000
-                 , bench "100000" $ nf bsSeq 1000000
-                 , bench "100000" $ nf bsSeq 10000000
+                 , bench "1000000" $ nf bsSeq 1000000
+                 , bench "10000000" $ nf bsSeq 10000000
                  ]
   ]
